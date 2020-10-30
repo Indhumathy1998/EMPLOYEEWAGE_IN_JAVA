d12469cbc99a822f0e890174b2983be3a0ea916d
Indhumathy1998 <indhumathymani12@gmail.com>
2020-10-30 14:51:46 +0530

    Added a branch UC-2_EMPLOYEE_DAILY_WAGE

--- Employeewage.java
+++ Employeewage.java
@@ -4,16 +4,23 @@ class Employeewage
         {
 
 	int IS_FULL_TIME = 1;
-
-	double empCheck = Math.floor(Math.random() * 10) % 2;
-
-	if(empCheck == IS_FULL_TIME)
-
-		System.out.println("Employee is present");
-
-	else
-
-		System.out.println("employee is absent");
-         }
+        int empRatePerHr;
+        int empHrs;
+	int salary;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+	if (empCheck == IS_FULL_TIME)
+        {
+	      empRatePerHr = 20;
+	      empHrs = 8;
+	      salary=empHrs*empRatePerHr;
+	      System.out.println(salary);
+	}
+       else
+        {
+				
+              salary = 0;
+	      System.out.println(salary);         
+        }
+        }
 
 }
