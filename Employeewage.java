7658e0e7bbcb10af3163dfc6e145953ff9c37ec6
Indhumathy1998 <73114841+Indhumathy1998@users.noreply.github.com>
2020-11-08 15:34:01 +0530

    Update Employeewage.java

--- Employeewage.java
+++ Employeewage.java
@@ -1,19 +1,15 @@
 class Employeewage
 {
-        public static void main(String[]args)
-        {
-
+   public static void main(String[]args)
+   {
 	int IS_FULL_TIME = 1;
-
-	double empCheck = Math.floor(Math.random() * 10) % 2;
-
-	if(empCheck == IS_FULL_TIME)
-
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+	   
+        if(empCheck == IS_FULL_TIME)
+	{
 		System.out.println("Employee is present");
-
-	else
-
-		System.out.println("employee is absent");
+        else
+                System.out.println("employee is absent");
          }
 
-}
+   }
